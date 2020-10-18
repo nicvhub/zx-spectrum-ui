7f8b05769a1fa79986f293caca2141ec8afc9cf1
Nicolas Venegas <zx128kok@gmail.com>
2020-10-18 11:31:26 -0400

    added big letter in top left of button

--- src/components/KeyboardButton/KeyboardButton.stories.tsx
+++ src/components/KeyboardButton/KeyboardButton.stories.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
 import KeyboardButton, { KeyboardButtonProps } from "./KeyboardButton";
-import { Story, Meta } from '@storybook/react/types-6-0';
 
 export default {
   component: KeyboardButton,
   title: "KeyboardButton",
 } as Meta;
 
-const Template: Story<KeyboardButtonProps>  = (args) => <KeyboardButton {...args}/>;
+const Template: Story<KeyboardButtonProps> = (args) => (
+  <KeyboardButton {...args} />
+);
 
 export const Default = Template.bind({});
 Default.args = {
--- src/components/KeyboardButton/KeyboardButton.tsx
+++ src/components/KeyboardButton/KeyboardButton.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import styles from "./KeyboardButton.module.css";
 
 export type KeyboardButtonProps = {
-  label: string
-}
-const KeyboardButton = ({label}: KeyboardButtonProps ) => {
+  label: string;
+};
+const KeyboardButton = ({ label }: KeyboardButtonProps): JSX.Element => {
+  const fiveCharacterTitle = label.slice(0, 5);
+  const firstCharacter = fiveCharacterTitle[0];
+
   return (
     <div className={styles.rubberButtonContainer}>
-      <button className={styles.rubberButton}>
-        <div className={styles.rubberButtonText}>{label}</div>
+      <button type="button" className={styles.rubberButton}>
+        <div
+          data-first-char={firstCharacter}
+          className={styles.rubberButtonText}
+        >
+          {fiveCharacterTitle}
+        </div>
       </button>
     </div>
   );
 };
-
 
 export default KeyboardButton;

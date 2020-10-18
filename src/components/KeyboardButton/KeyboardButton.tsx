e10c09ecba131f4c1913eb1dd1717412e4ce0f30
Nicolas Venegas <zx128kok@gmail.com>
2020-10-18 15:22:51 -0400

    red alternate text on key

--- src/components/KeyboardButton/KeyboardButton.tsx
+++ src/components/KeyboardButton/KeyboardButton.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import styles from "./KeyboardButton.module.css";
+import { ALPHABET } from "./types";
+import { KEY_TO_ALT } from "./const";
 
 export type KeyboardButtonProps = {
   label: string;
 };
+
 const KeyboardButton = ({ label }: KeyboardButtonProps): JSX.Element => {
   const fiveCharacterTitle = label.slice(0, 5);
-  const firstCharacter = fiveCharacterTitle[0];
-
+  const firstCharacter: ALPHABET | "" = fiveCharacterTitle[0]
+    ? (fiveCharacterTitle[0].toUpperCase() as ALPHABET)
+    : "";
+  const altKeyFunction: string = firstCharacter
+    ? KEY_TO_ALT[firstCharacter]
+    : "";
   return (
     <div className={styles.rubberButtonContainer}>
       <button type="button" className={styles.rubberButton}>
         <div
-          data-first-char={firstCharacter}
+          data-super-script={firstCharacter}
+          data-alt={altKeyFunction}
           className={styles.rubberButtonText}
         >
           {fiveCharacterTitle}
